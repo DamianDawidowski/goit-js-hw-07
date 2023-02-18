468d9f7f5a2eb033d5c8e1e23443601dbdfca731
Damian Dawidowski <damiandawidowski@gmail.com>
2023-02-18 19:22:16 +0100

    modified:   02-lightbox.html 	modified:   js/02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<a class=gallery__item href=${image.original}>
+      <img class="gallery__image" src=${image.preview} alt="${image.description}" />
+      </a>`
+  )
+
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", imageClick);
+
+function imageClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+ 
+    let galleryBox = new SimpleLightbox('.gallery a');
+    galleryBox.on('show.simplelightbox', function () {	// do something…
+
+        `<img src="${event.target.dataset.source}" width='800' height='800'>`
+    }
+    
+    );
+}
+
+ 
