3864722e37ea6f7e5f94f529c49ff50d8a47566e
Damian Dawidowski <damiandawidowski@gmail.com>
2023-02-18 17:12:01 +0100

    modified:   01-gallery.html 	modified:   js/01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<div class="gallery__item">
+      <a class=gallery__link href=${image.original}>
+      <img class="gallery__image" src=${image.preview} data-source=${image.original} alt="${image.description}" class=image   />
+      </a></div>`
+  )
+
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", imageClick);
+
+function imageClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width='800' height='800'>
+          `);
+  instance.show();
+  gallery.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+}
